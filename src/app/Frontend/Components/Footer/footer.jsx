1b447808828490b8ec564e58624075552d9ef41d
Jordanfery <j.fery@hotmail.fr>
2025-06-02 00:13:53 +0200

    ajout des réseaux sociaux

--- src/app/Frontend/Components/Footer/footer.jsx
+++ src/app/Frontend/Components/Footer/footer.jsx
@@ -1,14 +1,17 @@
-import { Facebook, Twitter } from 'lucide-react';
+import { Facebook, Linkedin, Instagram } from 'lucide-react';
 
 
 export default function Footer() {
     return (
-        <section className='left-0 h-16 w-full bg-[var(--color-bg1)] flex justify-evenly items-center px-4 z-50 shadow'>
+        <section className='left-0 h-16 w-full bg-[var(--color-bg1)] flex justify-center gap-4 items-center px-4 z-50 shadow'>
             <a href="https://www.facebook.com/kalelithos" className='text-[var(--color-title)] rounded-full'>
                 <Facebook />
             </a>
-            <a href="https://www.facebook.com/kalelithos" className='text-[var(--color-title)] rounded-full'>
-                <Twitter />
+            <a href="https://www.instagram.com/kalelithos/" className='text-[var(--color-title)] rounded-full'>
+                <Instagram />
+            </a>
+            <a href="https://www.linkedin.com/company/kalelithos/?originalSubdomain=fr" className='text-[var(--color-title)] rounded-full'>
+                <Linkedin />
             </a>
 
         </section>
